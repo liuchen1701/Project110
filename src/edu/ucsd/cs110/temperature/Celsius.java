31420790f75317f497733e8144276feaba9f5895
Chen Liu <liuchen1701@gmail.com>
2017-02-15 12:03:28 -0800

    Push

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,24 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by Leo on 2/15/2017.
+ */
+public class Celsius extends Temperature {
+    public Celsius(float t) {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
